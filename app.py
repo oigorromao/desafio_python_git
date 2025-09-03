ede22954fea93777bb65160bbf26d3c53dc41628
oigorromao <igor.faustino-youxlab@institutoyoux.org.br>
2025-09-03 17:52:50 -0300

    meu primeiro commit

---
+++ app.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome: ')
+idade = input ('Digite sua idade: ')
+linguagem = input ('Digite sua linguagem de programação favorita: ')
+print (f'Meu nome é {nome}, tenho {idade} anos é minha linguagem favorita é {linguagem}')
